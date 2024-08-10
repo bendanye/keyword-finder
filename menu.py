87437a08a52e2d3e6a4b5b38ae0c70ecca3f69c8
bendanye <shen_soonng@hotmail.com>
2024-08-10 14:02:09 +0800

    Allow to pass by one extra argument to replace the actual command with the value

--- menu.py
+++ menu.py
@@ -8,8 +8,8 @@ from typing import List
 from simple_term_menu import TerminalMenu
 
 
-def main(keyword: str, file_name: str = "help.json") -> None:
-    options = _options(keyword, file_name)
+def main(keyword: str, file_name: str = "help.json", extra_arg=None) -> None:
+    options = _options(keyword, file_name, extra_arg)
 
     terminal_menu = TerminalMenu(options, search_key=None)
     menu_entry_index = terminal_menu.show()
@@ -18,7 +18,7 @@ def main(keyword: str, file_name: str = "help.json") -> None:
         print(f"You have copied {options[menu_entry_index]}!")
 
 
-def _options(keyword: str, file_name: str) -> List[str]:
+def _options(keyword: str, file_name: str, extra_arg: str) -> List[str]:
     with open(file_name, "r") as json_file:
         helps = json.load(json_file)["helps"]
         for item in helps:
@@ -27,7 +27,7 @@ def _options(keyword: str, file_name: str) -> List[str]:
                 if type == "list":
                     return item[type]
                 elif type == "command":
-                    commands = item[type].split(" ")
+                    commands = item[type].replace("${1}", extra_arg).split(" ")
                     result = subprocess.run(commands, stdout=subprocess.PIPE, text=True)
                     # Split the output by newlines to create a list
                     output = result.stdout.split("\n")
@@ -39,7 +39,9 @@ def _options(keyword: str, file_name: str) -> List[str]:
 
 if __name__ == "__main__":
     if len(sys.argv) <= 2:
-        print("Specify file_name and keyword")
+        print("Specify file_name, keyword and/or additional argument")
         exit(1)
 
-    main(keyword=sys.argv[2], file_name=sys.argv[1])
+    extra_arg = None if len(sys.argv) == 3 else sys.argv[3]
+
+    main(keyword=sys.argv[2], file_name=sys.argv[1], extra_arg=extra_arg)
